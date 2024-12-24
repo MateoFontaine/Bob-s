5dc6b4ed503a62d554177e092e9dbe891ed95dd7
Mateo <mateofontaine7@gmail.com>
2024-12-24 11:38:23 -0300

    Le agrgamos animacion

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+      const splashScreen = document.getElementById('splash-screen');
+      splashScreen.style.transition = 'opacity 0.5s ease';
+      splashScreen.style.opacity = '0'; // Asegura que desaparezca suavemente después de la animación
+  
+      setTimeout(() => {
+        splashScreen.style.display = 'none'; // Oculta completamente el splash screen
+        const mainContent = document.getElementById('main-content');
+        mainContent.style.display = 'block'; // Muestra el contenido principal
+      }, 500); // Tiempo para el desvanecimiento
+    }, 1500); // 3000 ms = duración del splash screen
+  });
+  
