91847a3d4584c7e158cedb052d9044890b4e1455
Mateo <mateofontaine7@gmail.com>
2024-12-28 02:12:07 -0300

    Mejoramos la pantalla de carga 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
   const splashScreen = document.getElementById('splash-screen');
 
-  // Agregar una clase para la animación
+  // Agregar la clase para la animación de desaparición
   splashScreen.classList.add('hide');
 
-  // Esperar el final de la animación antes de ocultar completamente el splash screen
+  // Esperar a que termine la animación antes de ocultar completamente el splash screen
   splashScreen.addEventListener('animationend', () => {
     splashScreen.style.display = 'none'; // Oculta el splash screen
     const mainContent = document.getElementById('main-content');
