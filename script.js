7772fb5863a10a40c061fc649ed3706f52df10d6
Mateo <mateofontaine7@gmail.com>
2024-12-28 02:05:36 -0300

    Mejoramos la pantalla de carga

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', () => {
-    setTimeout(() => {
-      const splashScreen = document.getElementById('splash-screen');
-      splashScreen.style.transition = 'opacity 0.5s ease';
-      splashScreen.style.opacity = '0'; // Asegura que desaparezca suavemente después de la animación
+  const splashScreen = document.getElementById('splash-screen');
   
-      setTimeout(() => {
-        splashScreen.style.display = 'none'; // Oculta completamente el splash screen
-        const mainContent = document.getElementById('main-content');
-        mainContent.style.display = 'block'; // Muestra el contenido principal
-      }, 500); // Tiempo para el desvanecimiento
-    }, 1000); // 3000 ms = duración del splash screen
+  // Asegúrate de que todo está cargado antes de ocultar el splash screen
+  splashScreen.style.transition = 'opacity 0.5s ease';
+  splashScreen.style.opacity = '0'; // Desvanece el splash screen
+  
+  splashScreen.addEventListener('transitionend', () => {
+    // Cuando la transición termine, oculta completamente el splash screen
+    splashScreen.style.display = 'none';
+    const mainContent = document.getElementById('main-content');
+    mainContent.style.display = 'block'; // Muestra el contenido principal
   });
-  
+});
